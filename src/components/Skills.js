99f049c6a831600d0e7da2004178f1bc1b6e19a3
altamish <altamishpasha@gmail.com>
2023-02-04 00:38:23 +0530

    skills added

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import 'react-circular-progressbar/dist/styles.css';
 import Section from './Section';
-import {AiFillHtml5} from "react-icons/ai";
-import {SiCss3,SiTailwindcss} from "react-icons/si";
-import {BsFillBootstrapFill,BsWordpress} from "react-icons/bs";
-import {IoLogoJavascript} from "react-icons/io";
-import {FaReact,FaNodeJs} from "react-icons/fa";
-import {DiMongodb,DiGit} from "react-icons/di";
 import D3Icon from "../assets/d3.png"
+import html from "../assets/html-5.png";
+import css from "../assets/css-3.png";
+import bootstrap from "../assets/bootstrap.png";
+import javascript from "../assets/js.png";
+import git from "../assets/git.png";
+import nodejs from "../assets/nodejs.png";
+import reactjs from "../assets/react.png";
+import wordpress from "../assets/wordpress.png";
+import mongodb from "../assets/mongodb.png";
+import tailwindcss from "../assets/tailwind-css.png";
+
+const skills = [D3Icon,css,html,bootstrap,javascript,git,nodejs,reactjs,wordpress,mongodb,tailwindcss,]
 
 
 function Skills() {
@@ -16,18 +22,10 @@ function Skills() {
   return (
     <>
     <Section title="Skills">
-      <div className='flex justify-center items-center'>
-        <AiFillHtml5/>
-        <SiCss3/>
-        <IoLogoJavascript/>
-        <SiTailwindcss/>
-        <BsFillBootstrapFill/>
-        <BsWordpress/>
-        <FaReact/>
-        <FaNodeJs/>
-        <DiMongodb/>
-        <DiGit/>
-        <img src={D3Icon} alt="d3"/>
+      <div className='flex justify-center items-center gap-10 flex-wrap'>
+        {
+          skills.map((still,i) => <img key={i} src={still} alt={still} width={80}/>)
+        }
       </div>
     </Section>
     </>
