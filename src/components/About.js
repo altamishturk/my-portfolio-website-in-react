bb69417e8c911e00299adb3795e00780e3fc0329
altamish <altamishpasha@gmail.com>
2023-02-08 13:05:11 +0530

    contect form functionality completed

--- src/components/About.js
+++ src/components/About.js
@@ -3,15 +3,20 @@ import Section from "./Section";
 
 function About() {
   return (
-    <Section background="bg-gray-100" title="About">
+    <Section 
+    background="bg-gray-100" 
+    title="About"
+    >
        <div className="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
             <div className="md:5/12 lg:w-5/12">
               <img src="https://tailus.io/sources/blocks/left-image/preview/images/startup.png" alt="i" loading="lazy" width="" height=""/>
             </div>
             <div className="md:7/12 lg:w-6/12">
-              <h2 className="text-2xl text-gray-900 font-bold md:text-4xl">Nuxt development is carried out by passionate developers</h2>
-              <p className="mt-6 text-gray-600">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum omnis voluptatem accusantium nemo perspiciatis delectus atque autem! Voluptatum tenetur beatae unde aperiam, repellat expedita consequatur! Officiis id consequatur atque doloremque!</p>
-              <p className="mt-4 text-gray-600"> Nobis minus voluptatibus pariatur dignissimos libero quaerat iure expedita at? Asperiores nemo possimus nesciunt dicta veniam aspernatur quam mollitia.</p>
+              <h2 className="text-2xl text-gray-900 font-bold md:text-4xl">A Full-Stack Web Developer with a Passion for Quality Services</h2>
+              <p className="mt-6 text-gray-600">I'm a full stack web developer with over 2 years of experience in developing and managing web applications. My primary focus is on developing custom web applications using HTML, CSS and JavaScript, I am also proficient in a range of other technologies such as React, Node.js, and MongoDB.</p>
+              <p className="mt-4 text-gray-600">I am passionate about developing modern, responsive, and user-friendly websites. I have a deep understanding of the entire development process, from coding, debugging, and testing to deployment and maintenance. My goal is to develop innovative and user-centric web applications that meet customer needs.</p>
+              <p className="mt-4 text-gray-600">I'm highly organized and I work well under pressure. I'm comfortable working in a team as well as taking on individual projects. I'm able to quickly adapt to new tools and technologies and I'm highly motivated to continuously improve my skill set.</p>
+              <p className="mt-4 text-gray-600">With my extensive development experience and my dedication to delivering exceptional results, I am confident that I can help you build cutting-edge web applications. If you're looking for a full stack web developer, please get in touch. I would be more than happy to discuss your project and help you bring it to life.</p>
             </div>
           </div>
     </Section>
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,65 +1,121 @@
-import React, { useState } from 'react';
-import Section from './Section';
-
+import React, { useState } from "react";
+import Section from "./Section";
 
 function Contact() {
-  const [data,setData] = useState({name: "",email:"",message: ""});
+  const [data, setData] = useState({ name: "", email: "", message: "" });
+  const [sending, setSending] = useState(false);
+  const [sent, setSent] = useState(false);
 
-  const handleChange = (e)=> {
-    setData(prev => {
+  const handleChange = (e) => {
+    setData((prev) => {
       return {
         ...prev,
-        [e.target.name]: e.target.value
-      }
-    })
-  }
+        [e.target.name]: e.target.value,
+      };
+    });
+  };
 
-  const handleSubmit = async ()=> {
+  const handleSubmit = async () => {
     const url = "https://impossible-puce-loafers.cyclic.app/api/v1/email";
-
+    setSending(true);
     const res = await fetch(url, {
       method: "POST",
       headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
+        Accept: "application/json",
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify(data)
+      body: JSON.stringify(data),
     });
 
-    if(res.status === 200){
-      console.log(res);
+    setSending(false);
+
+    if (res.status === 200) {
+      setSent(true);
+      setTimeout(() => {
+        setSent(false);
+      }, 3000);
+      setData({ name: "", email: "", message: "" });
+    } else {
+      alert("someting went wrong");
     }
-  }
+  };
 
   return (
-    <Section background="bg-gray-100" title="Contact Us">
-       <div className="lg:w-1/2 md:w-2/3 mx-auto">
-      <div className="flex flex-wrap -m-2">
-        <div className="p-2 w-1/2">
-          <div className="relative">
-            <label htmlFor="name" className="leading-7 text-sm text-gray-600">Name</label>
-            <input value={data.name} onChange={handleChange} type="text" id="name" name="name" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+    <Section
+      background="bg-gray-100"
+      title="Contact Us"
+      description="If you would like to find out more about my web development services and how I can help you achieve your goals, please do not hesitate to get in touch. I am always available to answer any questions you may have, and I look forward to discussing your project in more detail."
+    >
+      <div className="lg:w-1/2 md:w-2/3 mx-auto">
+        {sent && (
+          <h4 className="text-center text-white bg-green-500 mb-5 py-3">
+            We got your message! get in touch with you soon
+          </h4>
+        )}
+
+        <div className="flex flex-wrap -m-2">
+          <div className="p-2 w-1/2">
+            <div className="relative">
+              <label htmlFor="name" className="leading-7 text-sm text-gray-600">
+                Name
+              </label>
+              <input
+                value={data.name}
+                onChange={handleChange}
+                type="text"
+                id="name"
+                name="name"
+                className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              />
+            </div>
           </div>
-        </div>
-        <div className="p-2 w-1/2">
-          <div className="relative">
-            <label htmlFor="email" className="leading-7 text-sm text-gray-600">Email</label>
-            <input value={data.email} onChange={handleChange} type="email" id="email" name="email" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+          <div className="p-2 w-1/2">
+            <div className="relative">
+              <label
+                htmlFor="email"
+                className="leading-7 text-sm text-gray-600"
+              >
+                Email
+              </label>
+              <input
+                value={data.email}
+                onChange={handleChange}
+                type="email"
+                id="email"
+                name="email"
+                className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              />
+            </div>
           </div>
-        </div>
-        <div className="p-2 w-full">
-          <div className="relative">
-            <label htmlFor="message" className="leading-7 text-sm text-gray-600">Message</label>
-            <textarea value={data.message} onChange={handleChange} id="message" name="message" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
+          <div className="p-2 w-full">
+            <div className="relative">
+              <label
+                htmlFor="message"
+                className="leading-7 text-sm text-gray-600"
+              >
+                Message
+              </label>
+              <textarea
+                value={data.message}
+                onChange={handleChange}
+                id="message"
+                name="message"
+                className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+              ></textarea>
+            </div>
           </div>
-        </div>
-        <div className="p-2 w-full">
-          <button onClick={handleSubmit} className="w-full text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
+          <div className="p-2 w-full">
+            <button
+              onClick={handleSubmit}
+              className="w-full text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+            >
+              {sending ? <span>Sending...</span> : <span>Sned</span>}
+            </button>
+          </div>
         </div>
       </div>
-    </div>
     </Section>
-  )
+  );
 }
 
-export default Contact
+export default Contact;
